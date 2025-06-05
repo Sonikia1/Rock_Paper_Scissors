6843d9f0e8a7cc2f30ed05ab1fcfe886501264fe
Nicholas Hughes <ndon89@yahoo.com>
2025-06-05 14:47:27 -0500

    Added counters for wins, losses, and ties. Added css to put pictures in the buttons, but only found a good one for rock.

--- rps.js
+++ rps.js
@@ -2,9 +2,13 @@ const rockBtn = document.getElementById("rock")
 const paperBtn = document.getElementById("paper")
 const scissorsBtn = document.getElementById("scissors")
 
-let compHand = null
-let playerHand = null
-let randomNum = null
+let compHand = null;
+let playerHand = null;
+let randomNum = null;
+
+let tieCounter = null;
+let playerWinCounter = null;
+let CompWinCounter = null;
 
   rockBtn.addEventListener("click", function() {
     playerHand = "R"
@@ -31,9 +35,9 @@ function randomHand(){
   console.log(randomNum)
   if (randomNum < 1/3) {
     compHand = "R"
-  } else if (randomNum >= 1/3 || randomNum < 2/3) {
+  } else if (randomNum >= 1/3 && randomNum < 2/3) {
     compHand = "P"
-  } else {
+  } else if (randomNum >= 2/3) {
     compHand = "S"
   }
   console.log(compHand)
@@ -42,22 +46,39 @@ function randomHand(){
 
 function winChecker() {
   if (playerHand == compHand) {
-    alert("It was a tie.")
+    alert("It was a tie.");
+    tieCounter += 1;
+    document.getElementById("ties").textContent = `Ties: ${tieCounter}` 
   }
   else if (playerHand == "R" && compHand == "S"){
-    alert("You have won.")
+    alert("You have won.");
+    playerWinCounter += 1;
+    document.getElementById("Wins").textContent = `Wins: ${playerWinCounter}` 
   } else if (playerHand == "R" && compHand == "P") {
     alert("The Computer has won.")
+    CompWinCounter += 1
+    document.getElementById("Losses").textContent = `Losses: ${CompWinCounter}`
   } else if (playerHand == "P" && compHand == "S"){
       alert("The Computer has won.")
+      CompWinCounter += 1
+      document.getElementById("Losses").textContent = `Losses: ${CompWinCounter}`
   } else if (playerHand == "P" && compHand == "R") {
       alert("You have won.")
+      playerWinCounter += 1;
+      document.getElementById("Wins").textContent = `Wins: ${playerWinCounter}` 
   } else if (playerHand == "S" && compHand == "R") {
       alert("The Computer has won.")
+      CompWinCounter += 1
+      document.getElementById("Losses").textContent = `Losses: ${CompWinCounter}`
   } else if (playerHand == "S" && compHand == "P") {
       alert("You have won.")
+      playerWinCounter += 1;
+      document.getElementById("Wins").textContent = `Wins: ${playerWinCounter}` 
   }
 
 }
+
+
+
 
 
