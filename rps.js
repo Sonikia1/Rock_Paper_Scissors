6d90849fb76240b51af513dfee8f7d7ad0ce2ac3
Nicholas Hughes <ndon89@yahoo.com>
2025-06-06 15:49:33 -0500

    Added autoplay and reset features. May create another branch and mess around with local storage.

--- rps.js
+++ rps.js
@@ -1,6 +1,10 @@
+
 const rockBtn = document.getElementById("rock")
 const paperBtn = document.getElementById("paper")
 const scissorsBtn = document.getElementById("scissors")
+const autoplayBtn = document.getElementById("auto-play")
+
+const resetBtn = document.getElementById("reset")
 
 let compHand = null;
 let playerHand = null;
@@ -9,6 +13,10 @@ let randomNum = null;
 let tieCounter = null;
 let playerWinCounter = null;
 let CompWinCounter = null;
+
+let playInterval = null
+
+let temp = 0
 
   rockBtn.addEventListener("click", function() {
     playerHand = "R"
@@ -26,6 +34,31 @@ let CompWinCounter = null;
     playerHand = "S"
     randomHand()
     winChecker()
+  });
+
+  autoplayBtn.addEventListener("click", function() {
+    console.log(`temp: ${temp}`)
+    if (temp === 0) {
+      temp +=1
+      playInterval = setInterval(autoPlay, 1000)
+    }
+  });
+
+
+  resetBtn.addEventListener("click", function() {
+    if (temp > 0) {
+      clearInterval(playInterval)
+      temp = 0
+      tieCounter = 0
+      playerWinCounter = 0
+      CompWinCounter = 0
+      document.getElementById("ties").textContent = "Ties: N/A"
+      document.getElementById("Wins").textContent = "Wins: N/A"
+      document.getElementById("Losses").textContent = "Losses: N/A"
+      document.getElementById("your-choice").textContent = ``
+      document.getElementById("comp-choice").textContent = ``
+
+    }
   })
 
 
@@ -40,44 +73,70 @@ function randomHand(){
   } else if (randomNum >= 2/3) {
     compHand = "S"
   }
-  console.log(compHand)
+  console.log(`CompHand: ${compHand}`)
+}
+
+function playerRandomHand(){
+  randomNum = Math.random()
+  console.log(randomNum)
+  if (randomNum < 1/3) {
+    playerHand = "R"
+  } else if (randomNum >= 1/3 && randomNum < 2/3) {
+    playerHand = "P"
+  } else if (randomNum >= 2/3) {
+    playerHand = "S"
+  }
+  console.log(`playerHand: ${playerHand}`)
 }
 
 
 function winChecker() {
   if (playerHand == compHand) {
-    alert("It was a tie.");
+    //("It was a tie.");
     tieCounter += 1;
     document.getElementById("ties").textContent = `Ties: ${tieCounter}` 
   }
   else if (playerHand == "R" && compHand == "S"){
-    alert("You have won.");
+    //("You have won.");
     playerWinCounter += 1;
     document.getElementById("Wins").textContent = `Wins: ${playerWinCounter}` 
   } else if (playerHand == "R" && compHand == "P") {
-    alert("The Computer has won.")
+    //("The Computer has won.")
     CompWinCounter += 1
     document.getElementById("Losses").textContent = `Losses: ${CompWinCounter}`
   } else if (playerHand == "P" && compHand == "S"){
-      alert("The Computer has won.")
+      //("The Computer has won.")
       CompWinCounter += 1
       document.getElementById("Losses").textContent = `Losses: ${CompWinCounter}`
   } else if (playerHand == "P" && compHand == "R") {
-      alert("You have won.")
+      //("You have won.")
       playerWinCounter += 1;
       document.getElementById("Wins").textContent = `Wins: ${playerWinCounter}` 
   } else if (playerHand == "S" && compHand == "R") {
-      alert("The Computer has won.")
+      //("The Computer has won.")
       CompWinCounter += 1
       document.getElementById("Losses").textContent = `Losses: ${CompWinCounter}`
   } else if (playerHand == "S" && compHand == "P") {
-      alert("You have won.")
+      //("You have won.")
       playerWinCounter += 1;
       document.getElementById("Wins").textContent = `Wins: ${playerWinCounter}` 
   }
 
+  document.getElementById("your-choice").textContent = `You: ${playerHand}`
+  document.getElementById("comp-choice").textContent = `Computer: ${compHand}`
+
 }
 
+
+function autoPlay() {
+  console.log("here");
+    temp +=1
+    randomHand();
+    playerRandomHand()
+    winChecker()
+  
+
+}
 
 
 
